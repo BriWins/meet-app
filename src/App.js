49ca4acef28974b99b55562f3fea3e331d23bca4
BriWins <95152254+BriWins@users.noreply.github.com>
2022-07-07 12:11:44 -0500

    updated code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getEvents, extractLocations } from './api';
+import './nprogress.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
-
 class App extends Component {
-  render() {
+
+  state = {
+    events: [],
+    locations: []
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+render() {
     return (
       <div className="App">
-        <EventList/>
-        <CitySearch/>
+        <EventList events={this.state.events}/>
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
         <NumberOfEvents/>
         </div>
     );
