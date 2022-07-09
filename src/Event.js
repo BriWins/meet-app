3f63ae553295190ecf95f6638893ebd21a29ff34
BriWins <95152254+BriWins@users.noreply.github.com>
2022-07-09 10:15:51 -0500

    update Event.js

--- src/Event.js
+++ src/Event.js
@@ -19,15 +19,15 @@ class Event extends Component {
   render() {
 
     const { event } = this.props;
-    
+
     return (
     <div className="event">
       <h4 className="title">{event.summary}</h4>
-      <p className="start-time"></p>
-      <p className="location"></p>
-      <p className="summary"></p>
-      <button className="btn-details" onClick={this.handleExpandDetail }></button>
-      <button className="btn-collapse" onClick={this.handleCloseDetail}></button>
+      <p className="start-time">{event.start}</p>
+      <p className="location">{event.location}</p>
+      <p className="summary">{event.description}</p>
+      <button className="btn-details" onClick={this.handleExpandDetail }>View Details</button>
+      <button className="btn-collapse" onClick={this.handleCloseDetail}>Close Details</button>
     </div>
     );
   }
