0f8ae36b40e02dde8e6f4fbefcc8e286214ef9b5
BriWins <95152254+BriWins@users.noreply.github.com>
2022-07-13 14:54:22 -0500

    parse date error

--- src/Event.js
+++ src/Event.js
@@ -16,6 +16,13 @@ class Event extends Component {
    this.setState({ collapsed: true }); 
   };
 
+  parseDate(date){
+    let eventDate = new Date(date);
+    let day = eventDate.toLocaleDateString((undefined, { timeZone: 'UTC' }));
+    let time = eventDate.getHours() + ":" +('0'+eventDate.getMinutes()).slice(-2);
+    return day +' |  '+time;
+  
+    }
   render() {
 
     const { event } = this.props;
@@ -23,7 +30,7 @@ class Event extends Component {
     return (
     <div className="event">
       <h4 className="title">{event.summary}</h4>
-      <p className="start-time"> {event.start.dateTime} ({event.start.timeZone})</p>
+      <p className="start-time">{this.parseDate(event.start.dateTime)} ({event.start.timeZone})</p>
       <p className="location">{event.location}</p>
       <p className="summary">{event.description}</p>
       <button className="btn-details" onClick={this.handleExpandDetail }>View Details</button>
