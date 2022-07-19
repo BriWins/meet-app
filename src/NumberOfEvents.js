f2738868edee062b3115ac2fca9bd53fee9fba74
BriWins <95152254+BriWins@users.noreply.github.com>
2022-07-19 10:00:50 -0500

    update handleUserInput()

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -4,29 +4,30 @@ import { ErrorAlert } from "./Alert";
 export class NumberOfEvents extends Component {
   state = {
     numberOfEvents: "",
+    userInput: "",
   };
 
   handleUserInput = (event) => {
     const value = event.target.value;
     if (!isNaN(value) && value >= 0 && value <= 32) {
-      this.setState({ numberOfEvents: value, ErrorText: "" });
+      this.setState({ numberOfEvents: value, errorAlert: "" });
     } else {
-      this.setState({ ErrorText: "Please select at least one event." });
+      this.setState({ errorAlert: "Select a number between 1 and 32" });
     } 
-
-    if ( value <= 0 || value > 32) {
-      this.setState({
-        numberOfEvents: "",
-        errorAlert: "Select a number between 1 and 32"
-      }); 
-    } else {
-        this.setState({
-          numberOfEvents: value,
-          errorAlert: ""
-        })
-      }
     this.props.updateEvents(undefined, value);
   };
+    // if ( value <= 0 || value > 32) {
+    //   this.setState({
+    //     numberOfEvents: "",
+    //     errorAlert: "Select a number between 1 and 32"
+    //   }); 
+    // } else {
+    //     this.setState({
+    //       numberOfEvents: value,
+    //       errorAlert: ""
+    //     })
+    //   }
+  
 
   render() {
     return (
