8edb8e2b017dca3568d438c5274f4b1b1f0f74e7
BriWins <95152254+BriWins@users.noreply.github.com>
2022-07-19 11:22:59 -0500

    update Event.test.js

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,57 +1,50 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Event from '../Event';
-import { mockData } from '../mock-data';
+import React from "react";
+import { shallow } from "enzyme";
+import Event from "../Event";
+import { mockData } from "../mock-data";
 
+describe("<Event/> component", () => {
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={mockData[0]} />);
+  });
 
-describe('<Event/> component', () => {
-   let EventWrapper;
-    beforeAll(() => {
-        EventWrapper = shallow(<Event event={mockData[0]} />);
-    });
+  test("render event", () => {
+    expect(EventWrapper.find(".event")).toHaveLength(1);
+  });
 
-    test('render event', () => {
-        expect(EventWrapper.find('.event')).toHaveLength(1);
-    });
+  test("render event title", () => {
+    expect(EventWrapper.find(".title")).toHaveLength(1);
+  });
 
-    test('render event title', () => {
-        expect(EventWrapper.find('.title')).toHaveLength(1);
-    });
+  test("render event start time", () => {
+    expect(EventWrapper.find(".start-time")).toHaveLength(1);
+  });
 
-    test('render event start time', () => {
-        expect(EventWrapper.find('.start-time')).toHaveLength(1);
-    });
+  test("render event location", () => {
+    expect(EventWrapper.find(".location")).toHaveLength(1);
+  });
+  test("render event summary", () => {
+    expect(EventWrapper.find(".title")).toHaveLength(1);
+  });
 
-    test('render event location', () => {
-        expect(EventWrapper.find('.location')).toHaveLength(1);
-    });
-    test('render event summary', () => {
-        expect(EventWrapper.find('.summary')).toHaveLength(1);
-    });
+  test("render expand details button", () => {
+    expect(EventWrapper.find(".btn-details")).toHaveLength(1);
+  });
 
-    test('render expand details button', () => {
-        expect(EventWrapper.find('.btn-details')).toHaveLength(1);
-    });
+  test("event is collapsed by default", () => {
+    expect(EventWrapper.state("collapsed")).toBe(true);
+  });
 
-    test('render hide details button', () => {
-        expect(EventWrapper.find('.btn-collapse')).toHaveLength(1);
-    });
+  test("expand details on click", () => {
+    EventWrapper.setState({ collapsed: false });
+    EventWrapper.find(".btn-details").simulate("click");
+    expect(EventWrapper.state("collapsed")).toBe(true);
+  });
 
-    test('event is collapsed by default', () => {
-        expect(EventWrapper.state('collapsed')).toBe(true);
-    });
-
-    test('expand details on click', () => {
-        EventWrapper.setState({ collapsed: false });
-        EventWrapper.find('.btn-details').simulate('click');
-        expect(EventWrapper.state('collapsed')).toBe(false);
-    });
-
-    test('collapse event details on click', () => {
-        EventWrapper.setState({ collapsed: false });
-        EventWrapper.find('.btn-collapse').simulate('click');
-        expect(EventWrapper.state('collapsed')).toBe(true);
-      });
-
-
+  test("collapse event details on click", () => {
+    EventWrapper.setState({ collapsed: false });
+    EventWrapper.find(".btn-details").simulate("click");
+    expect(EventWrapper.state("collapsed")).toBe(true);
+  });
 });
