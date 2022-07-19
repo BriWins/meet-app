8d79d2760a08a4e315842707f2ddc7ae7e632164
BriWins <95152254+BriWins@users.noreply.github.com>
2022-07-19 11:34:05 -0500

    updates

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -4,39 +4,54 @@ import { mount } from "enzyme";
 import NumberOfEvents from "../NumberOfEvents";
 import App from "../App";
 
-const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
+const feature = loadFeature("./src/features/specifyNumberOfEvents.feature");
 
 defineFeature(feature, (test) => {
-    let AppWrapper;
+  let AppWrapper;
 
-    test('When user hasn\'t specified a number, 32 is the default number.', ({ given, when, then }) => {
-        given('the user is on the main page of the app', async () => {
-            AppWrapper = mount(<App />);
-        });
-
-        when('the user hasn\'t specified a number of events', () => {
-            AppWrapper.update();
-        });
-
-        then('the default number of displayed events will be 32', () => {
-            expect(AppWrapper.find('.event')).toHaveLength(2);
-        });
+  test("When user hasn't specified a number, 32 is the default number.", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("the user is on the main page of the app", async () => {
+      AppWrapper = mount(<App />);
     });
 
-    test('User can change the number of events they want to see.', ({ given, when, then }) => {
-        given('the user is on the main page', async () => {
-            AppWrapper =  mount(<App />);
-        });
-
-        when('the user set a number of events he or she wants to see in the “Number of events” box', () => {
-            const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents)
-            NumberOfEventsWrapper.find('.user-number-input').simulate('change', { target: { value: 1 } });
-        });
-
-        then('this number of events will be displayed', () => {
-            const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents)
-            NumberOfEventsWrapper.find('.user-number-input').simulate('change', { target: { value: 1 } });
-            expect(AppWrapper.state('numberOfEvents')).toEqual(1)
-        });
+    when("the user hasn't specified a number of events", () => {
+      AppWrapper.update();
     });
+
+    then("the default number of displayed events will be 32", () => {
+      expect(AppWrapper.find(".event")).toHaveLength(2);
+    });
+  });
+
+  test("User can change the number of events they want to see.", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("the user is on the main page", async () => {
+      AppWrapper = mount(<App />);
+    });
+
+    when(
+      "the user set a number of events he or she wants to see in the “Number of events” box",
+      () => {
+        const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
+        NumberOfEventsWrapper.find("#user-number-input").simulate("change", {
+          target: { value: 1 },
+        });
+      }
+    );
+
+    then("this number of events will be displayed", () => {
+      const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
+      NumberOfEventsWrapper.find("#user-number-input").simulate("change", {
+        target: { value: 1 },
+      });
+      expect(AppWrapper.state("numberOfEvents")).toEqual(1);
+    });
+  });
 });
