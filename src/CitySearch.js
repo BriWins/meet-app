c99bb51298c060a608ad0b3bb60cce496db4e291
BriWins <95152254+BriWins@users.noreply.github.com>
2022-07-19 12:01:25 -0500

    updates

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,69 +1,80 @@
-import React, { Component } from 'react';
-import { InfoAlert } from './Alert';
+import React, { Component } from "react";
+import { InfoAlert } from "./Alert";
 
 class CitySearch extends Component {
-    state = {
-        query: '',
-        suggestions: [],
-        showSuggestions: undefined
-      }
+  state = {
+    query: "",
+    suggestions: [],
+    showSuggestions: undefined,
+  };
 
-      handleInputChanged = (event) => {
-        const value = event.target.value;
-        this.setState({showSuggestions:true});
-        const suggestions = this.props.locations.filter((location) => {
-          return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        });
-        if (suggestions.length === 0) {
-          this.setState({
-            query: value,
-            infoText: 'We can not find the city you are looking for. Please try another city',
-          });
-          } else {
-          return this.setState({
-            query: value,
-            suggestions,
-            infoText:''
-          });
-        }
-      };
-
-    handleItemClicked = (suggestion) => {
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ showSuggestions: true });
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    if (suggestions.length === 0) {
       this.setState({
-        query: suggestion,
-        suggestions: [],
-        showSuggestions:false,
-        infoText:''
+        query: value,
+        infoText:
+          "We can not find the city you are looking for. Please try another city",
       });
-      this.props.updateEvents(suggestion);
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: "",
+      });
     }
+  };
+
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+      suggestions: [],
+      showSuggestions: false,
+      infoText: "",
+    });
+
+    this.props.updateEvents(suggestion);
+  };
 
   render() {
     return (
       <div className="CitySearch">
-          <h1>Meet App</h1>
-          <h3>The place where learning meets the world!</h3>
+        <h1>Meet App</h1>
+        <h3>The place where learning meets the world!</h3>
 
-              <input
-              type="text"
-              className="city"
-              placeholder="search a city"
-              value={this.state.query}
-              onChange={this.handleInputChanged}
-              onFocus={() => { this.setState({ showSuggestions: true }) }} />
-
-    
-            <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
-                {this.state.suggestions.map((suggestion) => (
-                  <li key={suggestion}  onClick={() => this.handleItemClicked(suggestion)}>
-                  {suggestion}</li>
-                  ))}
-                
-            <li onClick={() => this.handleItemClicked("all")}>
-              <b>See all cities</b>
+        <input
+          type="text"
+          className="city"
+          placeholder="search a city"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+          onFocus={() => {
+            this.setState({ showSuggestions: true });
+          }}
+        />
+        <ul
+          className="suggestions"
+          style={this.state.showSuggestions ? {} : { display: "none" }}
+        >
+          {this.state.suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+              className="city-suggestion"
+            >
+              {suggestion}
             </li>
-            </ul>
-            
+          ))}
+
+          <li onClick={() => this.handleItemClicked("all")}>
+            <b>See all cities</b>
+          </li>
+        </ul>
+
         <InfoAlert text={this.state.infoText} />
       </div>
     );
