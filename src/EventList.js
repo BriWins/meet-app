ab146682425befeaef78877da470ea3a1998f059
BriWins <95152254+BriWins@users.noreply.github.com>
2022-07-20 10:20:52 -0500

    adding css styling

--- src/EventList.js
+++ src/EventList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Event from './Event';
 
+
 class EventList extends Component {
   render() {
     const { events } = this.props;
     // console.log(events);
     
     return (
+      
           <ul className="EventList">
             {events.map(event => 
               <li key={event.id}>
